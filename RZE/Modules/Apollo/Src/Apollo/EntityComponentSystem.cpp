49d8d04d704de7554a7134860197e9759ab8a995
Josh <covedrop@gmail.com>
2017-12-01 12:46:24 -0500

    some minor changes, have some design issues to think about..

--- RZE/Modules/Apollo/Src/Apollo/EntityComponentSystem.cpp
+++ RZE/Modules/Apollo/Src/Apollo/EntityComponentSystem.cpp
@@ -3,8 +3,17 @@
 namespace Apollo
 {
 
+	EntityComponentSystem::EntityComponentSystem()
+	{
+	}
+
+	EntityComponentSystem::~EntityComponentSystem()
+	{
+	}
+
 	void EntityComponentSystem::Initialize()
 	{
+		mEntities.reserve(150);
 	}
 
 	void EntityComponentSystem::Update()
--- RZE/Modules/Apollo/Src/Apollo/EntityComponentSystem.h
+++ RZE/Modules/Apollo/Src/Apollo/EntityComponentSystem.h
@@ -1,15 +1,38 @@
 #pragma once
 
+#include <vector>
+
 #include <Utils/Interfaces/SubSystem.h>
 
 namespace Apollo
 {
+	class IEntity;
+
 	class EntityComponentSystem : public ISubSystem
 	{
-	public:
+		typedef std::vector<IEntity*> EntityList;
 
+	public:
+		EntityComponentSystem();
+		virtual ~EntityComponentSystem();
+
+	// ISubSystem interface
+	public:
 		virtual void Initialize();
 		virtual void Update();
 		virtual void ShutDown();
+
+	// EntityComponentSystem API
+	public:
+		template<class TEntityType, typename... TArgs>
+		TEntityType* AddEntity(TArgs... args)
+		{
+			TEntityType* entity = new TEntityType(std::forward<TArgs>(args)...);
+			mEntities.push_back(entity);
+			return mEntities.back();
+		}
+
+	private:
+		EntityList mEntities;
 	};
 }
