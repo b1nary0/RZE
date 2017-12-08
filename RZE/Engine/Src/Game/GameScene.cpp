ef103aa70d32ce00012cefe3916d0626aca3f6e7
Josh <covedrop@gmail.com>
2017-12-08 12:37:52 -0500

    just adding some basic gamescene skeleton stuff

---
+++ RZE/Engine/Src/Game/GameScene.cpp
@@ -0,0 +1,28 @@
+#include <StdAfx.h>
+#include <Game/GameScene.h>
+
+#include <ECS/Systems/RenderSystem.h>
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+}
+
+void GameScene::Initialize()
+{
+}
+
+void GameScene::Start()
+{
+}
+
+void GameScene::Update()
+{
+}
+
+void GameScene::Finish()
+{
+}
--- RZE/Engine/Src/Game/GameScene.h
+++ RZE/Engine/Src/Game/GameScene.h
@@ -2,4 +2,15 @@
 
 class GameScene
 {
+public:
+	GameScene();
+	~GameScene();
+
+public:
+	virtual void Start();
+	virtual void Update();
+	virtual void Finish();
+
+public:
+	void Initialize();
 };
