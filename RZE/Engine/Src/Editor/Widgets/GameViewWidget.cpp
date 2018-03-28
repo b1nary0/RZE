10091074cf4c9ee4deb205144c9668d4d70eea2a
Josh ODonnell <covedrop@gmail.com>
2018-03-28 17:11:21 -0400

    no move and no collapse on game view for now

--- RZE/Engine/Src/Editor/Widgets/GameViewWidget.cpp
+++ RZE/Engine/Src/Editor/Widgets/GameViewWidget.cpp
@@ -29,7 +29,7 @@ void GameViewWidget::Display()
 	Vector2D size(static_cast<float>(mRTT->GetWidth()), static_cast<float>(mRTT->GetHeight()));
 	ImGui::SetNextWindowSize(ImVec2(size.X(), size.Y()));
 	ImGui::SetNextWindowPos(ImVec2(ImGui::GetIO().DisplaySize.x / 2, 20.0f), 0, ImVec2(0.5f, 0.0f)); // #TODO(Josh) Need to find solution for the magic numbers here (20.0f) which put this window below the main menu bar
-	if (ImGui::Begin("GameView", NULL, ImGuiWindowFlags_NoScrollbar)) // #TODO(Josh) Why does the window need scroll bar?
+	if (ImGui::Begin("GameView", NULL, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse)) // #TODO(Josh) Why does the window need scroll bar?
 	{
 		ImGui::Image((void*)mRTT->GetTextureID(), ImVec2(size.X(), size.Y()), ImVec2(0.0f, 1.0f), ImVec2(1.0f, 0.0f));
 
