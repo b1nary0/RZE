85f3bc063c8a006a20966f7d38a74573ca29a56b
Josh ODonnell <covedrop@gmail.com>
2018-03-28 14:22:54 -0400

    more reasonable RTT size

--- RZE/Engine/Src/Editor/Widgets/GameViewWidget.cpp
+++ RZE/Engine/Src/Editor/Widgets/GameViewWidget.cpp
@@ -8,24 +8,28 @@ GameViewWidget::GameViewWidget()
 
 GameViewWidget::~GameViewWidget()
 {
-
+	// #TODO(Josh) Really need to start the smart ptr pass to avoid stuff like this
+	if (mRTT)
+	{
+		delete mRTT;
+	}
 }
 
 void GameViewWidget::Initialize()
 {
 	mRTT = new Diotima::GLRenderTargetTexture();
-	mRTT->SetHeight(1920);
-	mRTT->SetWidth(1080);
+	mRTT->SetWidth(1280);
+	mRTT->SetHeight(720);
 
 	mRTT->Initialize();
 }
 
 void GameViewWidget::Display()
 {
-	ImGui::SetNextWindowSize(ImVec2(1920, 1080));
+	ImGui::SetNextWindowSize(ImVec2(mRTT->GetWidth(), mRTT->GetHeight()));
 	if (ImGui::Begin("GameView"))
 	{
-		ImGui::Image((void*)mRTT->GetTextureID(), ImVec2(1920, 1080));
+		ImGui::Image((void*)mRTT->GetTextureID(), ImVec2(mRTT->GetWidth(), mRTT->GetHeight()));
 
 		ImGui::End();
 	}
