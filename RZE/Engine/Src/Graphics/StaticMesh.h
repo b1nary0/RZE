55e0299ce3c0a6e5889c79e2bd3eb8bafadfdf23
Josh ODonnell <covedrop@gmail.com>
2019-01-14 13:17:32 -0800

    starting to break apart GFX* dependency engine-side
    this will fully decouple renderer-level concepts (GPU allocation, etc) from engine intent (higher level data descriptions)

---
+++ RZE/Engine/Src/Graphics/StaticMesh.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <Utils/PrimitiveDefs.h>
+
+#include <Utils/Math/Vector2D.h>
+#include <Utils/Math/Vector3D.h>
+
+class Material
+{
+
+};
+
+struct MeshVertex
+{
+	Vector3D Position;
+	Vector3D Normal;
+	Vector3D Tangent;
+	Vector2D UVData;
+};
+
+class StaticMesh
+{
+public:
+	StaticMesh();
+	~StaticMesh();
+
+	void AddVertex(const MeshVertex& vertex);
+	void AddIndex(U32 index);
+
+	void SetMaterial(Material* material);
+
+	const Material& GetMaterial() const;
+	const std::vector<U32>& GetIndices() const;
+	const std::vector<Vector3D>& GetPositions() const;
+	const std::vector<Vector3D>& GetNormals() const;
+	const std::vector<Vector2D>& GetUVCoords() const;
+
+	void OnLoadFinished();
+private:
+	Material* mMaterial;
+
+	std::vector<Vector3D> mPositions;
+	std::vector<Vector3D> mNormals;
+	std::vector<Vector3D> mTangents;
+	std::vector<Vector2D> mUVCoords;
+
+	std::vector<U32> mIndices;
+};
