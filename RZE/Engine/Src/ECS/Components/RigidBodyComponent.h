aa6657c98fe0608e22fa63f1e58c246ae9481bf5
Josh O'Donnell <covedrop@gmail.com>
2019-04-03 22:51:56 -0700

    rigidbody component

---
+++ RZE/Engine/Src/ECS/Components/RigidBodyComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Apollo/ECS/EntityComponent.h>
+
+class RigidBodyComponent final : public Apollo::Component<RigidBodyComponent>
+{
+public:
+
+};
--- RZE/Engine/Src/ECS/Systems/BulletPhysicsSystem.cpp
+++ RZE/Engine/Src/ECS/Systems/BulletPhysicsSystem.cpp
@@ -1,6 +1,8 @@
 #include <StdAfx.h>
 #include <ECS/Systems/BulletPhysicsSystem.h>
 
+#include <ECS/Components/RigidBodyComponent.h>
+
 BulletPhysicsSystem::BulletPhysicsSystem(Apollo::EntityHandler* const entityHandler)
 	: Apollo::EntitySystem(entityHandler)
 {
@@ -12,9 +14,15 @@ BulletPhysicsSystem::~BulletPhysicsSystem()
 
 void BulletPhysicsSystem::Initialize()
 {
+	InternalGetComponentFilter().AddFilterType<RigidBodyComponent>();
 }
 
 void BulletPhysicsSystem::Update(const std::vector<Apollo::EntityID>& entities)
+{
+
+}
+
+void BulletPhysicsSystem::ShutDown()
 {
 
 }
