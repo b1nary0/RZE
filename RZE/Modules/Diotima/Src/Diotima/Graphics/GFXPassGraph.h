f606434a854a6b3abf9d62c4f89650716d4ab552
Josh O'Donnell <covedrop@gmail.com>
2019-04-18 21:52:21 -0700

    Stubbing render pass ideas

---
+++ RZE/Modules/Diotima/Src/Diotima/Graphics/GFXPassGraph.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Diotima
+{
+	// GFXPassGraph is the systemic representation of GFXRenderPass.
+	// It provides the API to compile, prepare, and execute the frame graph,
+	// ?? ultimately culminating in the final frame output ??
+	class GFXPassGraph
+	{
+	public:
+		GFXPassGraph() = default;
+		~GFXPassGraph() = default;
+
+	private:
+	};
+}
--- RZE/Modules/Diotima/Src/Diotima/Graphics/RenderPass.h
+++ RZE/Modules/Diotima/Src/Diotima/Graphics/RenderPass.h
@@ -2,14 +2,51 @@
 
 #include <Diotima/Graphics/RenderTechnique.h>
 
+#include <Utils/PrimitiveDefs.h>
+
+#include <vector>
+
 namespace Diotima
 {
+	class RenderPassInput
+	{
+		std::vector<U32> BufferIDs;
+	};
+
+	// We could output to multiple buffers e.g gbuffer
+	class RenderPassOutput
+	{
+		std::vector<U32> BufferIDs;
+	};
+
+	// GFXRenderPass describes how a scene or subset of the scene will be drawn
+	// Takes as input either nothing or the result of a previous render pass.
+	// It will be expected that whoever defines the render pass understands the inputs
+	// and outputs necessary for the frame graph.
+	// A GFXRenderPass should be created for all rendering operations that culminate in
+	// an output.
+	//
+	//
+	//                                                 [DEPTH BUFFER]\
+	//                                                 [   GBUFFER  ] \
+	//       Depth Pass [DEPTH BUFFER] -> GBuffer Pass [   GBUFFER  ]  -> Lighting Pass [LIGHTING] -> BackBuffer
+	//                                                 [   GBUFFER  ]/
+	//
+	//
 	class GFXRenderPass
 	{
+	public:
+		GFXRenderPass() = default;
+		~GFXRenderPass() = default;
+
+	public:
+		void Execute() {}
 
 	private:
 		// Technique
 		GFXRenderTechnique mTechnique;
-		// List of meshes?
+
+		RenderPassInput* mInput;
+		RenderPassOutput* mOutput;
 	};
 }
