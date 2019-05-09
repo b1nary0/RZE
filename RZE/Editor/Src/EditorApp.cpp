9956697815eea9d405db73202414f0a52433e589
Josh O'Donnell <covedrop@gmail.com>
2019-05-08 20:42:13 -0700

    main menu bar

--- RZE/Editor/Src/EditorApp.cpp
+++ RZE/Editor/Src/EditorApp.cpp
@@ -21,7 +21,13 @@ void EditorApp::Start()
 
 void EditorApp::Update()
 {
-	ImGui::Text("WOLOLOLO");
+	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(5.0f, 5.0f));
+	if (ImGui::BeginMainMenuBar())
+	{
+		ImGui::MenuItem("Test");
+		ImGui::EndMainMenuBar();
+	}
+	ImGui::PopStyleVar();
 }
 
 void EditorApp::ShutDown()
