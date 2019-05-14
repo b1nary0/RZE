8892101aae9dbf224b23509a91c6099d5e44e468
Josh O'Donnell <covedrop@gmail.com>
2019-05-13 22:28:38 -0700

    rotation + scale but broken imgui

--- RZE/Engine/Src/ECS/Components/TransformComponent.cpp
+++ RZE/Engine/Src/ECS/Components/TransformComponent.cpp
@@ -6,8 +6,14 @@
 
 void TransformComponent::OnEditorInspect()
 {
-	float* values = const_cast<float*>(&Position.GetInternalVec().x);
+	float* positionValues = const_cast<float*>(&Position.GetInternalVec().x);
+	float* rotationValues = const_cast<float*>(&Rotation.GetInternalQuat().x);
+	float* scaleValues = const_cast<float*>(&Scale.GetInternalVec().x);
 
 	ImGui::Text("Position");
-	ImGui::DragFloat3("", values, 0.005f);
+	ImGui::DragFloat3("", positionValues, 0.005f);
+	ImGui::Text("Rotation");
+	ImGui::DragFloat3("", rotationValues, 0.005f);
+	ImGui::Text("Scale");
+	ImGui::DragFloat3("", scaleValues, 0.005f);
 }
