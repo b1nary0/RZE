a27c9359c7fe71ef14ca32e0c5e44cc6d251b154
Josh O'Donnell <covedrop@gmail.com>
2019-05-19 13:53:45 -0700

    different reflect model

--- RZE/Assets/Shaders/D3DTest_PS.hlsl
+++ RZE/Assets/Shaders/D3DTest_PS.hlsl
@@ -60,13 +60,13 @@ float CalculateBlinnPhong(float3 viewDir, float3 lightDir, float3 normal)
 {
 	float specular = 0.0f;
 	
-	float3 halfDir = normalize(lightDir + viewDir);
-	float specAngle = max(0.0f, dot(halfDir, reflect(-lightDir, normal)));
-	specular = pow(specAngle, materialData.Shininess);
+	//float3 halfDir = normalize(lightDir + viewDir);
+	//float specAngle = max(0.0f, dot(halfDir, reflect(-lightDir, normal)));
+	//specular = pow(specAngle, materialData.Shininess);
 
-	//float3 R = reflect(-lightDir, normal);
-	//float3 RdotV = max(0.0f, dot(R, viewDir)); 
-	//specular = pow(RdotV, materialData.Shininess);
+	float3 R = reflect(-lightDir, normal);
+	float3 RdotV = max(0.0f, dot(R, viewDir)); 
+	specular = pow(RdotV, materialData.Shininess);
 	
 	return specular;
 }
