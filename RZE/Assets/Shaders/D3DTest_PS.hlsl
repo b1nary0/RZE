7c7444fd6f9349f7561b28855b41d70f8d1b5124
Josh O'Donnell <covedrop@gmail.com>
2019-05-26 19:04:24 -0700

    no specular in shadow (shitty impl)

--- RZE/Assets/Shaders/D3DTest_PS.hlsl
+++ RZE/Assets/Shaders/D3DTest_PS.hlsl
@@ -168,6 +168,7 @@ float4 PSMain(PS_IN input) : SV_TARGET
 		float3 specularResult = specular * lightStrength * light.Color.rgb;
 		
 		float shadow = CalculateShadowFromDepthMap(light, input.FragPos, normal, lightDir);
+		specularResult *= 1.0f - shadow;
 
 		float3 result = (diffSample.rgb * ((ambientResult + diffuseResult * (1.0f - shadow))) + (specularResult * specularSample.rgb));
 		lightAccum += result;
