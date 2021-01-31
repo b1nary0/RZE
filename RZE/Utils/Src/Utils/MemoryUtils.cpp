92f49297d7855531f6b8fccb5c09d7fc3adadd64
Josh O'Donnell <covedrop@gmail.com>
2021-01-30 20:43:15 -0800

    store alignedsize in local during AlignSize

--- RZE/Utils/Src/Utils/MemoryUtils.cpp
+++ RZE/Utils/Src/Utils/MemoryUtils.cpp
@@ -8,5 +8,6 @@ U32 MemoryUtils::AlignSize(U32 size, U32 boundary)
 	AssertMsg((boundary & (boundary - 1)) == 0, "Boundary must be power of 2");
 
 	U32 bnd = boundary - 1;
-	return (size + bnd) & ~bnd;
+	U32 alignedSize = (size + bnd) & ~bnd;
+	return alignedSize;
 }
