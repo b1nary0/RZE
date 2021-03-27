bbea3326a1b5fdedae7c71c6840deb0df2245d62
Josh O'Donnell <covedrop@gmail.com>
2021-03-27 14:54:53 -0700

    "default" pixel shader

---
+++ RZE/Assets/Shaders/Pixel_Default_NewRenderer.hlsl
@@ -0,0 +1,28 @@
+struct PS_IN
+{
+	float4 Position : SV_POSITION;
+	float3 FragPos : POSITION;
+	float3 Normal : NORMAL;
+};
+
+float4 PSMain(PS_IN input) : SV_TARGET
+{
+	float LightStrength_Temp = 10.0f;
+	float3 LightPos_Temp = float3(0.0f, 5.0f, -20.0f);
+	float3 LightColour_Temp = float3(1.0f, 1.0f, 1.0f);
+	float3 Ambient_Temp = float3(0.1f, 0.1f, 0.1f);
+	float3 ObjectColour_Temp = float3(0.5f, 0.5f, 0.5f);
+
+	float3 lightDir = normalize(LightPos_Temp - input.FragPos);
+	
+	float3 shadingResult;
+	{
+		float diffuse = max(dot(lightDir, input.Normal), 0.0f);
+		float3 diffuseResult = diffuse * LightColour_Temp;
+		
+		shadingResult = (Ambient_Temp + diffuseResult) * ObjectColour_Temp;
+	}
+	
+	
+    return float4(shadingResult, 1.0f);
+}
