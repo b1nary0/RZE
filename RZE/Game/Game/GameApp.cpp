5299aacef25e8e8ad69ff063ceea2a2cf271967d
Josh O'Donnell <covedrop@gmail.com>
2022-03-09 18:22:34 -0800

    Revert "Move FinalRenderTargetStage creation responsibility to RenderEngine from Game"
    This reverts commit 782ef317c5890d435d89a8d067bd4c6e5b8e1054.

--- RZE/Game/Game/GameApp.cpp
+++ RZE/Game/Game/GameApp.cpp
@@ -46,6 +46,8 @@ void GameApp::Initialize()
 #ifdef IMGUI_ENABLED
 	RZE::GetRenderEngine().AddRenderStage<ImGuiRenderStage>();
 #endif
+
+	RZE::GetRenderEngine().AddRenderStage<FinalRenderTargetStage>();
 }
 
 void GameApp::Start()
