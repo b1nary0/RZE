d953ca0853de53203e648121a92c02055a8a1b55
jpod <covedrop@gmail.com>
2023-08-31 19:46:45 -0400

    Revert "MaterialAssetWriter"
    This reverts commit 50a1daa787088b4e62cc94857ffea2832dcad501.

--- RZE/SourceAssetBurner/Src/SourceAssetBurner/Importers/AssimpSourceImporter.h
+++ RZE/SourceAssetBurner/Src/SourceAssetBurner/Importers/AssimpSourceImporter.h
@@ -3,7 +3,6 @@
 #include <SourceAssetBurner/Importers/SourceImporter.h>
 
 #include <Asset/AssetImport/MeshAssetWriter.h>
-#include <Asset/AssetImport/MaterialAssetWriter.h>
 
 #include <Utils/PrimitiveDefs.h>
 
@@ -23,11 +22,37 @@ public:
 	virtual bool Import(const Filepath& filePath) override;
 
 private:
+	// #TODO
+	// standardize these data structures so they dont all need to be maintained...
+	struct MaterialData
+	{
+		enum ETextureFlags
+		{
+			TEXTUREFLAG_NONE = 0,
+			TEXTUREFLAG_ALBEDO = 1 << 0,
+			TEXTUREFLAG_SPECULAR = 1 << 1,
+			TEXTUREFLAG_NORMAL = 1 << 2,
+			TEXTUREFLAG_ALL = TEXTUREFLAG_ALBEDO | TEXTUREFLAG_SPECULAR |  TEXTUREFLAG_NORMAL
+		};
+
+		std::string MaterialName;
+		struct MaterialProperties
+		{
+			float Shininess = 0.0f;;
+			float Opacity = 1.0f;
+		} Properties;
+		// #TODO These flags inform which texture types are used by this material
+		// probably not the best implementation of this but i think the asset infrastructure
+		// needs to mature.
+		U8 TextureFlags = TEXTUREFLAG_NONE;
+		std::vector<std::string> TexturePaths;
+	};
+
 	void ProcessNode(const aiNode& node, const aiScene& scene);
 	void ProcessMesh(const aiMesh& mesh, const aiScene& scene, MeshData& outMeshData);
 
 	bool WriteMeshAsset();
-	bool WriteMaterialAsset(const MaterialData& materialData);
+	bool WriteMaterialAsset(const std::string& relativePath, const MaterialData& materialData);
 	bool WriteTextureAsset();
 	
 private:
