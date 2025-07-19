4c8ba2b202f3603afe4af87564dcd3af77db5311
JPOD <covedrop@gmail.com>
2025-07-18 18:44:06 -0700

    remove unnecessary fields in RenderCommand_UploadDataToBuffer

--- RZE/Modules/Rendering/Src/Rendering/RenderCommand.h
+++ RZE/Modules/Rendering/Src/Rendering/RenderCommand.h
@@ -185,8 +185,6 @@ namespace Rendering
 
 		const void* data = nullptr;
 		ConstantBufferHandle bufferHandle;
-		const char* name = nullptr;
-		size_t dataSize = 0;
 	};
 
 	struct RenderCommand_ReleaseVertexShader : RenderCommand
