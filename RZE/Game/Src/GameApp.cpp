1c34fe98209bb9e234717d5eb2ec7b66c20e2e1e
JPOD <covedrop@gmail.com>
2025-08-08 19:00:29 -0700

    dont know why this was commented out..

--- RZE/Game/Src/GameApp.cpp
+++ RZE/Game/Src/GameApp.cpp
@@ -120,26 +120,26 @@ void GameApp::RegisterInputEvents(InputHandler& inputHandler)
 
 bool GameApp::ProcessInput(const InputHandler& handler)
 {
-//#ifdef IMGUI_ENABLED
-//	ImGuiIO& io = ImGui::GetIO();
-//
-//	const Vector2D& mousePos = handler.GetProxyMouseState().CurPosition;
-//	const Vector2D& prevMousePos = handler.GetProxyMouseState().PrevPosition;
-//	io.MousePos = ImVec2(mousePos.X(), mousePos.Y());
-//	io.MousePosPrev = ImVec2(prevMousePos.X(), prevMousePos.Y());
-//
-//	for (U32 mouseBtn = 0; mouseBtn < 3; ++mouseBtn)
-//	{
-//		io.MouseDown[mouseBtn] = handler.GetProxyMouseState().CurMouseBtnStates[mouseBtn];
-//	}
-//
-//	for (int key = 0; key < MAX_KEYCODES_SUPPORTED; ++key)
-//	{
-//		io.KeysDown[key] = handler.GetProxyKeyboardState().IsDownThisFrame(key);
-//	}
-//
-//	return false;
-//#endif
+#ifdef IMGUI_ENABLED
+	ImGuiIO& io = ImGui::GetIO();
+
+	const Vector2D& mousePos = handler.GetProxyMouseState().CurPosition;
+	const Vector2D& prevMousePos = handler.GetProxyMouseState().PrevPosition;
+	io.MousePos = ImVec2(mousePos.X(), mousePos.Y());
+	io.MousePosPrev = ImVec2(prevMousePos.X(), prevMousePos.Y());
+
+	for (U32 mouseBtn = 0; mouseBtn < 3; ++mouseBtn)
+	{
+		io.MouseDown[mouseBtn] = handler.GetProxyMouseState().CurMouseBtnStates[mouseBtn];
+	}
+
+	for (int key = 0; key < MAX_KEYCODES_SUPPORTED; ++key)
+	{
+		io.KeysDown[key] = handler.GetProxyKeyboardState().IsDownThisFrame(key);
+	}
+
+	return false;
+#endif
 
 	return RZE_Application::ProcessInput(handler);
 }
